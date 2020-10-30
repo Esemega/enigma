1a28ba2ad4b6c994bae20256514f5430564c375b
Esemega <sogaral89@gmail.com>
2020-10-30 19:59:32 +0100

    removing console.logs, transforming text to lowercase, adding listener to buttons and using textareas to get message and display translations

--- app.js
+++ app.js
@@ -2,11 +2,6 @@
 var plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
 var encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
 
-var message =
-  "b': yqg ,:agr(hue:) shrerg jq,u'qf q !hg ,:psqñrf:g rg,fuwurae: s:f r' ,yq!x mbg:z 'q 'r,yrn !hfua( t'usqfuq ,:apu(:)m z q ,:a!uahq,u:a rajuq ha pragqir ,utfqe:n br' dhr !h dhurfqg)";
-var message2 =
-  "¡lo has conseguido! puedes vacilar a tus compañeros escribiendo por el chat: '¡soy la leche, turing fliparia conmigo!' y a continuacion envia un mensaje cifrado, ¡el que tu quieras!";
-
 // var searchCharIntoString = (char, string) => {
 //   for (var i = 0; i < string.length; i++) {
 //     if (string[i] === char) return i;
@@ -15,11 +10,12 @@ var message2 =
 // };
 
 var getTranslation = (message, startingAlphabet, finishingAlphabet ) => {
+  var lowerCaseMessage = message.toLowerCase();
   var indexOfLetter;
   var translatedLetter = "";
   var translatedMessage = "";
 
-  for (var letter of message) {
+  for (var letter of lowerCaseMessage) {
     // indexOfLetter = searchCharIntoString(letter, startingAlphabet);
     //Optional part -> Using indexOf 
     indexOfLetter = startingAlphabet.indexOf(letter);
@@ -31,7 +27,29 @@ var getTranslation = (message, startingAlphabet, finishingAlphabet ) => {
   return translatedMessage;
 };
 
-var decrytedMessage = getTranslation(message, encryptedAlphabet, plainAlphabet);
-console.log(decrytedMessage);
-console.log(message);
-console.log(getTranslation(decrytedMessage, plainAlphabet, encryptedAlphabet));
+var buttonClickListener = (event) => {
+    var messageToEncryptTextarea = document.getElementById("message-to-encrypt");
+    var messageToDecryptTextarea = document.getElementById("message-to-decrypt");
+    var messageToEncrypt = messageToEncryptTextarea.value;
+    var messageToDecrypt = messageToDecryptTextarea.value;
+
+    if (event.target.id === "encrypt-button") {
+        
+        var messageEncrypted = getTranslation(messageToEncrypt, plainAlphabet, encryptedAlphabet);
+
+        messageToDecryptTextarea.value = messageEncrypted;
+    } else {
+        var messageDecrypted = getTranslation(messageToDecrypt, encryptedAlphabet, plainAlphabet);
+
+        messageToEncryptTextarea.value = messageDecrypted;
+    }
+
+
+}
+
+
+var encryptButton= document.getElementById("encrypt-button");
+var decryptButton= document.getElementById("decrypt-button");
+
+encryptButton.addEventListener('click', buttonClickListener);
+decryptButton.addEventListener('click', buttonClickListener);
